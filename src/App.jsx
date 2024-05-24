d724b00186fde130a5a6cd39626e20f086c730b0
Mukesh <dasriyamukesh87@gmail.com>
2024-05-24 18:42:25 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+// src/App.js
 
-import './App.css'
+import './App.css';
 import QRCode from "react-qr-code";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductPage from './ProductPage';
 
 function App() {
-
+  const productUrl = "http://localhost:3000/product?name=DummyProduct&description=This%20is%20a%20dummy%20product%20description.";
 
   return (
-    <>
-    <QRCode value="hey" /> 
- 
-    </>
-  )
+    <Router>
+      <div className="App">
+        <QRCode value={productUrl} />
+        <Routes>
+          <Route path="/product" element={<ProductPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
